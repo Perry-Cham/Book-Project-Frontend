808da10a0f618dac40c624f4e477ded58219a836
Perry Chamileke <pchamileke@gmail.com>
2025-08-28 19:47:35 +0200

    Added a set goal function to allow the user to set reading goals

--- src/routes/components/modal.jsx
+++ src/routes/components/modal.jsx
@@ -1,36 +1,64 @@
-import {useState, useEffect} from 'react'
-import {useLocation} from 'react-router-dom'
-import {XMarkIcon} from '@heroicons/react/24/solid'
-function Modal({modalState, setPage}){
+import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+function Modal({ modalState }) {
   const [open, setOpen] = useState(true)
   const condition = location.pathname === "/goal" || location.pathname === "/study";
   const api = import.meta.env.VITE_API
-function handleClose(){
-  setOpen(false)
-}
-function handleSubmit(e){
-  e.preventDefault()
-  const form = new FormData(e.target)
-  const data = form.get("pageCount")
-  setPage(data)
-}
+  function handleClose() {
+    setOpen(false)
+  }
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (modalState.type !== "setGoal") {
+      const form = new FormData(e.target)
+      const data = form.get("pageCount")
+      modalState.sendData(data)
+    } else {
+      const form = new FormData(e.target)
+     const data = {
+        numberOfBooks: form.get("numberOfBooks"),
+        duration: form.get("duration"),
+        unit: form.get("unit")
+      }
+      modalState.sendData(data);
+    }
 
-  return(
+  }
+
+  return (
     open && <div className="w-[100vw] h-[100vh] bg-black bg-opacity-75 fixed top-0 left-0 flex justify-center items-center">
 
-     <div className="p-[1.75rem] bg-white rounded-md md:w-[300px] min-w-[200px]">
-        <XMarkIcon onClick={handleClose} className="size-6 cursor-pointer text-right"/>
+      <div className="p-[1.75rem] bg-white rounded-md md:w-[300px] min-w-[200px]">
+        <XMarkIcon onClick={handleClose} className="size-6 cursor-pointer text-right" />
         <h2>{modalState.title}</h2>
         <p>{modalState.message}</p>
-        
-        {condition &&
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <label htmlFor="pageCount">{modalState.type === "setMax" ? "Enter Page Count (if different from the one stated)" : "Enter The Page You're On Now"}</label>
-          <input className="border-2 rounded-md focus:border-blue-800 p-2" type="number" name="pageCount"/>
-          <button className="btn-secondary rounded-md mt-3"  type="submit">Set Book</button>
-        </form>}
+
+        {(condition && modalState.type == "setMax" || modalState.type == "setCurrent") &&
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <label htmlFor="pageCount">{modalState.type === "setMax" ? "Enter Page Count (if different from the one stated)" : "Enter The Page You're On Now"}</label>
+            <input className="border-2 rounded-md focus:border-blue-800 p-2" type="number" name="pageCount" />
+            <button className="btn-secondary rounded-md mt-3" type="submit">Set Book</button>
+          </form>}
+
+        {modalState.type == "setGoal" &&
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <h2>Set a Reading Goal</h2>
+            <label htmlFor="">Number of Books</label>
+            <input className="block" type="Number" name="numberOfBooks" value="3"/>
+            <label htmlFor="">Duration</label>
+            <input className="block" type="Number" name="duration"/>
+            <label htmlFor="unit">Unit Of Measurement</label>
+            <br></br>
+          <input className="capitalize" type="radio" name="unit"  id="day" value="day"/>
+          <label for="day">Day(s)</label><br/>
+          <input className="capitalize" type="radio" name="unit"  id="week" value="week"  selected />
+          <label for="week">Week(s)</label><br/>
+            <button className="btn-primary">Submit</button>
+          </form>
+        }
       </div>
     </div>
-    )
+  )
 }
 export default Modal;
--- src/routes/goal.jsx
+++ src/routes/goal.jsx
@@ -7,73 +7,100 @@ function Goal_Page() {
   const [sbooks, setSbooks] = useState();
   const [cbooks, setCbooks] = useState();
   const [goal, setGoal] = useState();
+  const user = useAuthStore((state) => state.user)
+  const setUser = useAuthStore((state) => state.setUser)
   const [modalState, setModalState] = useState({
-    title:"",message:"",type:"",open:false
+    title: "", message: "", type: "", open: false
   });
   const [currBook, setCurrBook] = useState({})
   useEffect(() => {
     getSavedBooks()
     getCurrentBooks();
   }, [])
-  const user = useAuthStore((state) => state.user)
-  const setUser = useAuthStore((state) => state.setUser)
+
+
   //Upon saved Book onClick
-  function openModal(book, type) {
-    setCurrBook(book)
-    setModalState({...modalState, type:type, open:true})
+  function openModal(book, type, fun) {
+    book && setCurrBook(book)
+    setModalState({ ...modalState, type: type, open: true, sendData: fun })
   }
 
+  //Functions that send data to the server
   function setPage(pageCount) {
-    setModalState({...modalState, open:false})
+    setModalState({ })
     let data;
-    
-  if(modalState.type === 'setMax'){
-    data = {
-      title: currBook.title,
-      cover: currBook.cover,
-      pageCount: pageCount || currBook.pageCount,
-      page: 1,
-      mainBook: currBook._id,
-      history: []
-    }}else {
+
+    if (modalState.type === 'setMax') {
+      data = {
+        title: currBook.title,
+        cover: currBook.cover,
+        pageCount: pageCount || currBook.pageCount,
+        page: 1,
+        mainBook: currBook._id,
+        history: []
+      }
+    } else {
       data = {
         id: currBook._id,
-        title:currBook.title,
-        pageCount:pageCount
+        title: currBook.title,
+        pageCount: pageCount
       }
     }
     handleRead(data)
   }
 
-
-
-  //To be executed on component mount
-  async function handleRead(book) {
+  //Post Requests
+  async function handleRead(data) {
     const endpoint = modalState.type == "setMax" ? "setcurrentbook" : "setcurrentpage";
     try {
-      const response = await axios.post(`${api}/${endpoint}`, book, {
+      const response = await axios.post(`${api}/${endpoint}`, data, {
         withCredentials: true
       })
       if (response.status == 200) {
-     if(modalState.type === "setMax"){alert(`Congratulations you've just started ${response.data.name}`)}else{
-       alert(`Congratulations you're now on page ${response.data.page} in ${response.data.title}`)
-     }
+        if (modalState.type === "setMax") { alert(`Congratulations you've just started ${response.data.name}`) } else {
+          alert(`Congratulations you're now on page ${response.data.page} in ${response.data.title}`)
+        }
         getCurrentBooks();
         getSavedBooks();
+        setModalState({})
       }
-      else {alert('Error adding book')
+      else {
+        alert('Error adding book')
         console.log(response)
       }
     } catch (err) {
       console.error(err)
     }
   }
+
+
+  async function handleGoal(data) {
+    console.log(data)
+    setModalState({})
+     try {
+        const response = await axios.post(`${api}/setgoal`, data, {
+          withCredentials: true
+        })
+        if (response.status == 200) {
+          alert("Congratulations you've just started a new goal. Happy Reading")
+       }
+        else {alert('Error adding book')
+          console.log(response)
+        }
+          setModalState({})
+        }catch (err) {
+        console.error(err)
+      } 
+  }
+
+
+  //Functions that fetch data from the server
   async function getCurrentBooks() {
     try {
       const response = await axios.get(`${api}/getcurrentbooks`, {
         withCredentials: true
       })
-      if(response.status == 200){
+      if (response.status == 200) {
         response.data.length !== 0 ? setCbooks(response.data.currentBooks) : setCbooks(undefined)
         console.log(response)
       }
@@ -91,7 +118,7 @@ function Goal_Page() {
     try {
       const response = await axios.get(`${api}/getsavedbooks`, { withCredentials: true })
       if (response.status == 200) {
-        response.data.length !== 0? setSbooks(response.data):setSbooks(undefined)
+        response.data.length !== 0 ? setSbooks(response.data) : setSbooks(undefined)
       } else {
         console.log(response)
         alert('Error fetching books')
@@ -106,7 +133,7 @@ function Goal_Page() {
       const response = await axios.get(`${api}/getcurrentbooks`, {
         withCredentials: true
       })
-      if(response.status == 200){
+      if (response.status == 200) {
         response.data.currentBooks.length !== 0 ? setCbooks(response.data.currentBooks) : setCbooks(undefined)
         console.log(response.data)
       }
@@ -119,85 +146,86 @@ function Goal_Page() {
       alert('Error fetching books')
     }
   }
-
-  async function handleDelete(id,saved){
+  //Functions that delete data from the server
+  async function handleDelete(id, saved) {
     const isSaved = saved;
-    if(isSaved){
-      try{
-        const response =  await axios.delete(`${api}/deletesaved/${id}`,{
-          withCredentials:true
+    if (isSaved) {
+      try {
+        const response = await axios.delete(`${api}/deletesaved/${id}`, {
+          withCredentials: true
         })
-        if(response.status == 200){
+        if (response.status == 200) {
           alert("You have deleted the book successfully")
           getSavedBooks()
-        }else{
+        } else {
           alert("Server side error")
         }
-      }catch(err){
+      } catch (err) {
         console.error(err)
         alert("Server side error")
       }
 
-    }else{
-      try{
-        const response = await axios.delete(`${api}/deletecurrent/${id}`,{
-          withCredentials:true
+    } else {
+      try {
+        const response = await axios.delete(`${api}/deletecurrent/${id}`, {
+          withCredentials: true
         })
-        if(response.status == 200){
+        if (response.status == 200) {
           alert("You have deleted the book successfully")
           getCurrentBooks()
-        }else{
+        } else {
           console.log(response)
           alert("Server side error current")
         }
-      }catch(err){
+      } catch (err) {
         console.error(err)
         alert("Server side error current")
       }
     }
   }
+
   return (
     <>
       <h2>Hello, {user.name && user.name}</h2>
-      {modalState.open && <Modal modalState={modalState} setPage={setPage} />}
+      {modalState.open && <Modal modalState={modalState} />}
       <section>
-      <h2>Currently Reading</h2>
-      {cbooks ? cbooks.map((book) =>
-        <div key={book._id}>
-        <img className="w-[150px]" src={book.cover} alt={book.title + " cover image"} />
-        <p>{book.title}</p>
-        <p>{book.author}</p>
-        <p>{book.pageCount}</p>
-        <button onClick={() => openModal(book, "setCurrent")} className="btn-primary">Set Current Page</button>
-        <button className="btn-secondary" onClick={() => handleDelete(book._id, false)}>Delete Book</button>
-        </div>) :
-        <div>
-        <p>You haven't started reading anything yet, books you read will appear here</p>
-        </div>}
-        </section>
+        <h2>Currently Reading</h2>
+        {cbooks ? cbooks.map((book) =>
+          <div key={book._id}>
+            <img className="w-[150px]" src={book.cover} alt={book.title + " cover image"} />
+            <p>{book.title}</p>
+            <p>{book.author}</p>
+            <p>{book.pageCount}</p>
+            <button onClick={() => openModal(book, "setCurrent")} className="btn-primary">Set Current Page</button>
+            <button className="btn-secondary" onClick={() => handleDelete(book._id, false)}>Delete Book</button>
+          </div>) :
+          <div>
+            <p>You haven't started reading anything yet, books you read will appear here</p>
+          </div>}
+      </section>
 
-        <section>
+      <section>
         <h2>Saved Books</h2>
         {sbooks ? sbooks.map((book) =>
           <div key={book._id}>
-          <img className="w-[150px]" src={book.cover} alt={book.title + " cover image"} />
-          <p>{book.title}</p>
-          <p>{book.author}</p>
-          <p>{book.pageCount}</p>
-          <button className="btn-primary" onClick={() => openModal(book, "setMax")}>Start Reading</button>
-          <button className="btn-secondary" onClick={() => handleDelete(book._id,true)}>Delete Book</button>
+            <img className="w-[150px]" src={book.cover} alt={book.title + " cover image"} />
+            <p>{book.title}</p>
+            <p>{book.author}</p>
+            <p>{book.pageCount}</p>
+            <button className="btn-primary" onClick={() => openModal(book, "setMax")}>Start Reading</button>
+            <button className="btn-secondary" onClick={() => handleDelete(book._id, true)}>Delete Book</button>
           </div>) :
           <div>
-          <p>You haven't saved anything yet, books you save will appear here</p>
+            <p>You haven't saved anything yet, books you save will appear here</p>
           </div>}
-          </section>
+      </section>
 
       <section>
         <h2>Reading Goals</h2>
         {goal ? <div>Place holder</div> :
           <div>
             <p>Build a reading habit by setting a reading goal. Once you set one it'll appear here</p>
-            <button>Set Goal</button>
+            <button className="btn-primary" onClick={() => openModal("", "setGoal", handleGoal)}>Set Goal</button>
           </div>
         }
       </section>
